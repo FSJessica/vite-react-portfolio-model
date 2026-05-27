605fda18470af29229ac3096fb73f333c803d7f1
Jessica Ferreira <fjes.ks@gmail.com>
2026-05-27 18:26:48 -0300

    Components created

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,28 @@
 
 import './App.css'
-
+import About from './components/About';
+import Header from './components/Header';
+import ProjectList from './components/ProjectList';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+ 
 function App() {
-  const name = "John Doe";
-  const profession = "Full Stack Developer";
-  const projects = [{
-    title: "Project One",
-    description: "A web application built using React and Node.js",
-    link: "#"
-  },
-  {
-    title: "Project Two",
-    description: "An eCommerce platform built using Django",
-    link: "#"
-  }
-]
-
   return (
     <div className= "App">
       {/* Header section */}
-      <header className="header">
-        <h1>{name}</h1>
-        <p>{profession}</p>
-        <nav>
-          <a href='#about'>  About</a>
-          <a href='#projects'>  Projects</a>
-          <a href='#contact'>  Contact</a>
-        </nav>
-      </header>
+      <Header />
 
       {/* About Section */}
-      <section id= 'about' className='about-section'>
-        <h2>About Me</h2>
-        <p>Hello! I am {name}, a passionate {profession}. I love building web application that solves real world user problems.</p>
-      </section>
+      <About />
 
       {/* Projects Section */}
-      <section id= 'projects' className='projects-section'>
-        <h2> Projects</h2>
-        <div className='projects-list'>
-          {projects.map((project, index) =>  (
-            <div key= {index} className='project-item'>
-              <h3>{project.title}</h3>
-              <h3>{project.description}</h3>
-              <a href={project.link} target='_blank' rel="noopener noreferrer"> ViewProject</a>
-            </div>
-          ))}
-        </div>
-      </section>
+      <ProjectList />
 
       {/* Contact Section*/}
-      <section id='contact' className='contact-section'>
-        <h2>Contact Me</h2>
-        <p>If you would like to get in touch, feel free to email me at <a href='mailto:johndoe@example.com'> johndoe@example.com</a></p>
-      </section>
+      <Contact />
 
       {/* Footer Section */}
-      <footer className='footer'>
-        <p>2025. All right Reserved</p>
-      </footer>
-
+      <Footer />
     </div>
   );
 
